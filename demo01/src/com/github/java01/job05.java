90e02e6b167c5415ecab66d1995f4585d11d7269
leilvlong <1455013965@qq.com>
2019-03-21 15:18:58 +0800

    5. 多线程交替执行

---
+++ demo01/src/com/github/java01/job05.java
@@ -0,0 +1,67 @@
+package com.github.java01;
+
+import java.util.ArrayList;
+
+/*
+开启三条线程交替执行:
+    使用标识标记状态来进行执行
+ */
+
+
+public class job05 {
+    static int tag=1;
+    public static void main(String[] args) {
+        new Thread(()->{
+            while(true){
+                synchronized (job05.class){
+                    while(tag != 1){
+                        try {
+                            job05.class.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(Thread.currentThread().getName()+": "+tag);
+                    tag = 2;
+                    job05.class.notifyAll();
+                }
+            }
+        }).start();
+
+        new Thread(()->{
+            while(true){
+                synchronized (job05.class){
+                    while(tag != 2){
+                        try {
+                            job05.class.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(Thread.currentThread().getName()+": "+tag);
+                    tag = 3;
+                    job05.class.notifyAll();
+                }
+            }
+        }).start();
+
+        new Thread(()->{
+            while(true){
+                synchronized (job05.class){
+                    while(tag != 3){
+                        try {
+                            job05.class.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(Thread.currentThread().getName()+": "+tag);
+                    tag = 1;
+                    job05.class.notifyAll();
+                }
+            }
+        }).start();
+    }
+}
+
+
