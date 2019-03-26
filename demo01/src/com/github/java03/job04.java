0969762238d069064abfb89fbf51da9440b24804
leilvlong <1455013965@qq.com>
2019-03-26 11:03:46 +0800

    I/O常用类 缓冲流读写快原因简单分析 数组链表简单分析

--- demo01/src/com/github/java03/job04.java
+++ demo01/src/com/github/java03/job04.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.Objects;
 
 /*
+
 I/O的使用虽然简便  但是对应的类很多 使用起来各具特点:
     字节流:
         FileInputStream      输入字节流(read())
@@ -32,6 +33,38 @@ I/O的使用虽然简便  但是对应的类很多 使用起来各具特点:
         OutputStreamWriter   输出转换流
 输出流中:
     构造中有可选项 append(true为续写 false为覆盖(默认项))
+
+缓冲流读写文件为何会比单个字符读写快很多(实现原理):
+    文件读写操作硬盘:
+        单个字节的读与写操作会增加大量的硬盘开销
+        而缓冲流则将多个字节读入内存中的数组上再写出到硬盘
+        通过操作内存减少了硬盘读写的开销
+        而操作内存是比硬盘快很多的
+如何通过数组一次性读取写出多个字节:
+    1. 一次性写入多个字节到数组(实现方式不知 最关键的两步)
+    2. 遍历数组写入字节值(数组查询快)) 操作内存
+    3. 遍历数组写取出存入的字节值
+    4  将数组中的所有字节值一次写入到硬盘(实现方式不知 最关键的两步)
+    因为数组特性的原因(所以会选择数组做缓冲流) 所以缓冲流会有flush方法:
+        1. 有一个数组写入  [1,2,3,4,5]
+        2. 写出
+        3. 在此写入 [6,7,8,9,10]
+        4.再次写出.....
+        这个过程只需遍历就可以操作,不需要其他多余的操作
+        当进行文件读写操作时:
+            逻辑判断当数组写满后如果依然有后续字节写入
+            就写出到文件 (为防止覆盖数组原先字节值所导致文件内容丢失)
+            再重头(索引0)继续写入字节
+数组特点:
+    连续的内存空间查询只需遍历
+        所以查询快
+    因为是连续的内存空间,所以要进行增删时需对整个连续的内存块进行操作
+    插入删除的时候，就要把修改的那个节点之后的所有数据都向后移动，或者向前移动。所以就慢了
+链表特点:
+    查询慢 增删快
+    链表中的数据在内存中是松散的，每一个节点都有一个指针指向下一个节点，
+    这样查找起来就比较慢了
+    而插入删除的时候就是断开一个节点，然后插入删除之后再接起来
  */
 public class job04 {
     public static void main(String[] args) throws Exception {
@@ -40,7 +73,8 @@ public class job04 {
         //fun3();
         //fun4();
         //fun5();
-        fun6();
+        //fun6();
+        fun7();
     }
 
     /*
@@ -154,6 +188,18 @@ public class job04 {
             fol.write(chars,0,len);
         }
         fol.close();
+        fil.close();
+    }
+
+    public static void fun7() throws IOException{
+        // 验证不做写 单做读的速度
+        FileInputStream fil = new FileInputStream("demo01\\funs\\mmm.txt");
+
+        long statr = System.currentTimeMillis();
+        for(byte[] bytes=new byte[1024*8];fil.read(bytes) != -1;){
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(end-statr);
         fil.close();
     }
 
