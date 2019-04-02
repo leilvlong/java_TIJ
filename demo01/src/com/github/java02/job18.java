ff50ff6a850cd9d65d750b94ac51b73bf9fd07b1
leilvlong <1455013965@qq.com>
2019-04-02 22:06:23 +0800

    final 与静态

--- demo01/src/com/github/java02/job18.java
+++ demo01/src/com/github/java02/job18.java
@@ -12,9 +12,16 @@ final修饰变量的一些细节:
     成后该数据一直存于静态的内存空间,随时调用,相对的也会更消耗内存资源,而一般成员则在被
     调用时初始化数据后生存于堆上,失去依赖后会被清理,更加动态的使用系统内存资源
     正是这个原因 不允许static final 修饰的空白常量的存在
+
+构造器也是static方法,尽管并没有显示的static修饰
+每个类的编译代码都存于它自己的独立文件中,该文件
+只在需要使用程序代码时才会被加载(所以说java的加载原则是
+不分静态或一般成员,只在使用时才会加载)
+可以说,类的加载是在其任何静态成员被访问时加载(因为只能直接访问类的静态成员
+非静态成员只能通过new 对象,而现在已经得知构造方法也是静态的 )
  */
 
-public class job18 {
+public class  job18 {
     public static void main(String[] args) {
 
     }
@@ -24,7 +31,11 @@ final class FinalClass{
     final int index;
     static final int count = 10;
     public FinalClass(int index) {
-        this.index = index;
+        this.index = fun();
+    }
+
+    public static int fun(){
+        return 10;
     }
 }
 
