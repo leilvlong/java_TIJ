e8d6b22a46f621babec916f99ac40ce658a8cc0d
leilvlong <1455013965@qq.com>
2019-04-09 18:59:19 +0800

    Jdbc template简单自定义

--- demo02/src/com/javaJdbc/druid/myTemplat/MyTemplatTest.java
+++ demo02/src/com/javaJdbc/druid/myTemplat/MyTemplatTest.java
@@ -2,15 +2,24 @@ package com.javaJdbc.druid.myTemplat;
 
 import com.javaJdbc.druid.JdbcUtil;
 
+import javax.swing.tree.RowMapper;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 
 /*
 测试 JdbcTemplate Test
  */
 public class MyTemplatTest {
-    public static void main(String[] args) throws SQLException {
-        MyTemplate jdbcTemplate = new MyTemplate(JdbcUtil.getDataSource());
+    public static void main(String[] args) throws SQLException, InvocationTargetException, IllegalAccessException, InstantiationException {
+        /*MyTemplate jdbcTemplate = new MyTemplate(JdbcUtil.getDataSource());
         String sql = "insert into studen value(0,?,?,?)";
-        jdbcTemplate.update(sql,"周八",22,1);
+        jdbcTemplate.update(sql,"周八",22,1);*/
+
+        MyTemplate jdbcTemplate = new MyTemplate(JdbcUtil.getDataSource());
+        String sql = "select name from studen where id=1";
+        if(jdbcTemplate.query(sql, String.class) instanceof String) {
+            System.out.println(jdbcTemplate.query(sql, String.class));
+        }
+
     }
 }
--- demo02/src/com/javaJdbc/druid/myTemplat/MyTemplate.java
+++ demo02/src/com/javaJdbc/druid/myTemplat/MyTemplate.java
@@ -2,11 +2,21 @@ package com.javaJdbc.druid.myTemplat;
 
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ParameterMetaData;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import javax.swing.tree.RowMapper;
+import javax.swing.tree.TreePath;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.TypeVariable;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+/*
+模拟JDBC TempLate
+    Update(实现增删改操作)
+    query(实现查询单数据(单条某列))
+
+ */
 public class MyTemplate {
     private DataSource dataSource;
 
@@ -14,7 +24,7 @@ public class MyTemplate {
         this.dataSource = dataSource;
     }
 
-    public int update(String sql,Object ...args) throws SQLException {
+    public int update(String sql, Object... args) throws SQLException {
         //获取连接
         Connection conn = dataSource.getConnection();
         // 预编译sql语句
@@ -22,11 +32,11 @@ public class MyTemplate {
         // 获取元选项并设置sql语句的参数
         ParameterMetaData psmd = ps.getParameterMetaData();
         int valueCount = psmd.getParameterCount();
-        if(valueCount != args.length){
+        if (valueCount != args.length) {
             throw new SQLException("Value incoming error");
         }
         for (int i = 1; i <= valueCount; i++) {
-            ps.setObject(i,args[i-1]);
+            ps.setObject(i, args[i - 1]);
         }
         // 执行增删改等更新数据库操作
         int i = ps.executeUpdate();
@@ -37,4 +47,30 @@ public class MyTemplate {
         // 返回受影响行数
         return i;
     }
+
+    public <T> T query(String sql, Class<T> type, Object ...args) throws SQLException,IllegalAccessException, InstantiationException {
+        //获取连接
+        Connection conn = dataSource.getConnection();
+        // 预编译sql语句
+        PreparedStatement ps = conn.prepareStatement(sql);
+        // 获取元选项并设置sql语句的参数
+        ParameterMetaData psmd = ps.getParameterMetaData();
+        int valueCount = psmd.getParameterCount();
+        if (valueCount != args.length) {
+            throw new SQLException("Value incoming error");
+        }
+        for (int i = 1; i <= valueCount; i++) {
+            ps.setObject(i, args[i - 1]);
+        }
+
+        T t = type.newInstance();
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()){
+            t = (T) rs.getObject(1);
+        }
+        return null;
+
+    }
+
 }
+
