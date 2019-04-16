61c3b8474ee1b4ffaefdd9b3ef9d8683850e0136
leilvlong <1455013965@qq.com>
2019-04-16 16:59:50 +0800

    jdbc template 自定义

--- demo02/src/com/javaJdbc/druid/myTemplat/MyTemplatTest.java
+++ demo02/src/com/javaJdbc/druid/myTemplat/MyTemplatTest.java
@@ -21,29 +21,37 @@ public class MyTemplatTest {
 
 
         MyTemplate jdbcTemplate = new MyTemplate(JdbcUtil.getDataSource());
-        String sql = "select gender from studen where id=10";
 
-/*       Long query1 =  jdbcTemplate.query(sql, Long.class);
-        Integer query2 =  jdbcTemplate.query(sql, Integer.class);
-        Float query3 =  jdbcTemplate.query(sql, Float.class);
-        Double query4 =  jdbcTemplate.query(sql, Double.class);
-        String query5 =  jdbcTemplate.query(sql, String.class);
-       System.out.println("LONG :"+query1);
-       System.out.println("INTEGER :"+query2);
-       System.out.println("FLOAT :"+query3);
-       System.out.println("DOUBLE :"+query4);
-       System.out.println("STRING :"+query5);*/
+        /*String sql1 = "select age from studen where id=?";
+        Long query1 = jdbcTemplate.query(sql1, Long.class);
+        Integer query2 = jdbcTemplate.query(sql1, Integer.class);
+        Float query3 = jdbcTemplate.query(sql1, Float.class);
+        Double query4 = jdbcTemplate.query(sql1, Double.class);
+        String query5 = jdbcTemplate.query(sql1, String.class);
+        System.out.println("LONG :" + query1);
+        System.out.println("INTEGER :" + query2);
+        System.out.println("FLOAT :" + query3);
+        System.out.println("DOUBLE :" + query4);
+        System.out.println("STRING :" + query5);*/
 
-        Long query1 =  jdbcTemplate.query(sql, Long.class);
-        Integer query2 =  jdbcTemplate.query(sql, Integer.class);
-        Float query3 =  jdbcTemplate.query(sql, Float.class);
-        Double query4 =  jdbcTemplate.query(sql, Double.class);
-        String query5 =  jdbcTemplate.query(sql, String.class);
+        /*String sql2 = "select age from studen where id=?";
+        Long query1 =  jdbcTemplate.query(sql2, Long.class,1);
+        Integer query2 =  jdbcTemplate.query(sql2, Integer.class,1);
+        Float query3 =  jdbcTemplate.query(sql2, Float.class,1);
+        Double query4 =  jdbcTemplate.query(sql2, Double.class,1);
+        String query5 =  jdbcTemplate.query(sql2, String.class,1);
         System.out.println("LONG :"+query1);
         System.out.println("INTEGER :"+query2);
         System.out.println("FLOAT :"+query3);
         System.out.println("DOUBLE :"+query4);
-        System.out.println("STRING :"+query5);
+        System.out.println("STRING :"+query5);*/
+
+        String sql3 = "select * from studen where id=1 ";
+        System.out.println(jdbcTemplate.queryForObject(sql3, Studen.class));
+
+        String sql4 = "select * from studen  ";
+        System.out.println(jdbcTemplate.queryForObjects(sql4, Studen.class));
+
     }
 
 
--- demo02/src/com/javaJdbc/druid/myTemplat/MyTemplate.java
+++ demo02/src/com/javaJdbc/druid/myTemplat/MyTemplate.java
@@ -1,8 +1,12 @@
 package com.javaJdbc.druid.myTemplat;
 
 // 利用反射 对各包装数据类型进行优化
+import jdk.swing.interop.SwingInterOpUtils;
+
 import javax.sql.DataSource;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.*;
 import java.util.*;
 
@@ -47,66 +51,78 @@ public class MyTemplate {
 
     public int update(String sql, Object... args) throws SQLException {
         //获取连接
-        Connection conn = dataSource.getConnection();
-        // 预编译sql语句
-        PreparedStatement ps = conn.prepareStatement(sql);
-        // 获取元选项并设置sql语句的参数
-        ParameterMetaData psmd = ps.getParameterMetaData();
-        int valueCount = psmd.getParameterCount();
-        if (valueCount != args.length) {
-            throw new SQLException("Value incoming error");
-        }
-        for (int i = 1; i <= valueCount; i++) {
-            ps.setObject(i, args[i - 1]);
-        }
+        Connection conn = getMyConnection();
+        // 对sql语句操作
+        PreparedStatement ps = getPreparedStatement(conn,sql, args);
         // 执行增删改等更新数据库操作
         int i = ps.executeUpdate();
         // 关闭资源
-        ps.close();
-        conn.close();
+        closeConnection(conn,ps);
 
         // 返回受影响行数
         return i;
     }
 
     public <T> T query(String sql, Class<T> type, Object ...args) throws Exception {
-        //获取连接
-        Connection conn = dataSource.getConnection();
-        // 预编译sql语句
-        PreparedStatement ps = conn.prepareStatement(sql);
-
-        // 获取元选项并设置sql语句的参数
-        ParameterMetaData psmd = ps.getParameterMetaData();
-        int valueCount = psmd.getParameterCount();
-        if (valueCount != args.length) {
-            throw new SQLException("Value incoming error");
-        }
-        if (args.length != 0) {
-            for (int i = 1; i <= valueCount; i++) {
-                ps.setObject(i, args[i - 1]);
-            }
-        }
-
-
-        // 对查询集结果处理
+        // 获取连接
+        Connection conn = getMyConnection();
+        // 对sql语句操作
+        PreparedStatement ps = getPreparedStatement(conn,sql, args);
+        // 获取查询集
         ResultSet rs = ps.executeQuery();
-
+        // 获取数据
         T data = (T) getData(rs, type);
         if(data == null){
             throw new SQLException("Get value is null");
         }
-
+        //关闭资源
+        closeConnection(conn,ps,rs);
+        //返回数据
         return data;
     }
 
+    public <T> T queryForObject(String sql,Class<T> object, Object... args) throws SQLException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        // 获取连接
+        Connection conn = getMyConnection();
+        // 对sql语句操作
+        PreparedStatement ps = getPreparedStatement(conn,sql, args);
+        // 获取查询集
+        ResultSet rs = ps.executeQuery();
+        // 获取对象
+        T data = getObject(rs,object);
+        //关闭资源
+        closeConnection(conn,ps,rs);
+        //返回对象
+        return data;
+    }
+
+    public <T> List<T> queryForObjects (String sql,Class<T> object, Object... args) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        // 获取连接
+        Connection conn = getMyConnection();
+        // 对sql语句操作
+        PreparedStatement ps = getPreparedStatement(conn,sql, args);
+        // 获取查询集与结果集行数
+        ResultSet rs = ps.executeQuery();
+        rs.last();
+        int count = rs.getRow();
+        rs.beforeFirst();
+
+        // 容器 与获取数据
+        ArrayList<T> datas = new ArrayList<>();
+        T data=null;
+        while (count>0){
+            data = getObject(rs,object);
+            datas.add(data);
+            count--;
+        }
+        return datas;
+    }
 
     private <T> Object getData(ResultSet rs, Class<T> type) throws Exception {
         Object data = null;
         if (rs.next()){
-
             //查询数据
             Object selectData =  rs.getObject(1);
-            System.out.println(selectData.getClass().getSimpleName());
             List<String> dataType = Arrays.asList(dataTypes);
             if(dataType.contains(type.getName()) && dataType.contains(selectData.getClass().getName())){
                 while (true){
@@ -160,7 +176,58 @@ public class MyTemplate {
         return t;
     }
 
+    private <T> T getObject(ResultSet rs,Class<T> object) throws SQLException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, InstantiationException {
+        T t = object.getConstructor().newInstance();
+        //获取对象属性
+        Field[] declaredFields = object.getDeclaredFields();
+        if (rs.next()) {
+            Object rsData = null;
+            for (Field declaredField : declaredFields) {
+                declaredField.setAccessible(true);
+                String name = declaredField.getName();
+                try{
+                    rsData = rs.getObject(name);
+                }catch (Exception e){
+                    throw new SQLException("Object property with table property inconformity");
+                }
+                declaredField.set(t, rsData);
+            }
+        }
+        return t;
+    }
 
 
+    private PreparedStatement getPreparedStatement(Connection conn,String sql, Object[] args) throws SQLException {
+        // 预编译sql语句
+        PreparedStatement ps = conn.prepareStatement(sql);
+
+        // 获取元选项并设置sql语句的参数
+        ParameterMetaData psmd = ps.getParameterMetaData();
+        int valueCount = psmd.getParameterCount();
+        if (valueCount != args.length) {
+            throw new SQLException("Value incoming error");
+        }
+        if (args.length != 0) {
+            for (int i = 1; i <= valueCount; i++) {
+                ps.setObject(i, args[i - 1]);
+            }
+        }
+        return ps;
+    }
+
+    private Connection getMyConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    private void closeConnection(Connection conn, PreparedStatement ps, ResultSet rs) throws SQLException {
+        rs.close();
+        ps.close();
+        conn.close();
+    }
+
+    private void closeConnection(Connection conn,PreparedStatement ps) throws SQLException {
+        ps.close();
+        conn.close();
+    }
 }
 
