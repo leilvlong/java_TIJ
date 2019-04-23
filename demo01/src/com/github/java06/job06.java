95d5aff8b7edd8388606d8b91dadf08d00d513a3
leilvlong <1455013965@qq.com>
2019-04-23 11:44:38 +0800

    接口允许继承

--- demo01/src/com/github/java06/job06.java
+++ demo01/src/com/github/java06/job06.java
@@ -1,10 +1,17 @@
 package com.github.java06;
 /*
-接口也允许继承接口
-当被类实现时
-实现类应当重写所有的方法(接口继承的基类接口的抽象方法)
-以下案例又将是方法重写与重载出现在同一个类中,但这次将不用担心带来问题
-因为接口继承基类接口的方法,当导出接口重载基类接口的方法时应当被视为一个新方法由实现类实现
+接口也允许继承接口:
+    当被类实现时实现类应当重写所有的方法(接口继承的基类接口的抽象方法与自身的方法)
+    以下案例又将是方法重写与重载出现在同一个类中，
+但这次将不用担心带来问题。
+    因为接口继承基类接口的方法，当导出接口重载基类
+接口的方法时应当被视为一个导出接口自身的方法由实现
+类实现
+    而接口是不能实现方法的,因此不存在重写方法然后被
+实现类继承带来冲突这种问题
+
+接口继承接口只为扩展更多态多样的的接口行为规范
+
  */
 public class job06 {
 }
@@ -12,15 +19,14 @@ public class job06 {
 
 interface Inte{
     Object fun();
-    String fun2();
+    String fun(Integer str);
 }
 
 
 interface Inter extends Inte{
     Object fun(String string);
 
-    @Override
-    String fun2();
+
 }
 
 
@@ -32,12 +38,14 @@ class Shi implements Inter{
     }
 
     @Override
-    public Object fun(String string) {
+    public String fun(Integer str) {
         return null;
     }
 
     @Override
-    public String fun2() {
+    public Object fun(String string) {
         return null;
     }
+
+
 }
