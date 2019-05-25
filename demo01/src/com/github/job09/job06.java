76c3101e3419e01f2c2082689e5bbfedb83e5caa
leilvlong <1455013965@qq.com>
2019-05-25 20:34:01 +0800

    异常捕获

---
+++ demo01/src/com/github/job09/job06.java
@@ -0,0 +1,33 @@
+package com.github.job09;
+/*
+获取异常栈:
+    第一个为抛出异常的方法名
+ */
+public class job06 {
+    public static void main(String[] args) {
+        fun();
+        System.out.println("-------------------------------");
+        gun();
+        System.out.println("-------------------------------");
+        dun();
+
+    }
+
+    public static void fun(){
+        try{
+            throw new Exception();
+        }catch (Exception e){
+            for (StackTraceElement stackTraceElement : e.getStackTrace()) {
+                System.out.println(stackTraceElement.getMethodName());
+            }
+        }
+    }
+
+    public static void gun(){
+        fun();
+    }
+
+    public static void dun(){
+        gun();
+    }
+}
