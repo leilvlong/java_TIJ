b1b07b1188b7c47e61d4130bcdca4ccfa52302aa
leilvlong <1455013965@qq.com>
2019-05-25 20:09:20 +0800

    自定义异常

---
+++ demo01/src/com/github/job09/job05.java
@@ -0,0 +1,20 @@
+package com.github.job09;
+
+public class job05 {
+    public static void main(String[] args) {
+        try{
+            fun();
+        }catch (Exception e){
+
+            StackTraceElement[] stackTrace = e.getStackTrace();
+            for (StackTraceElement stackTraceElement : stackTrace) {
+                System.out.println(stackTraceElement);
+            }
+        }
+    }
+
+    public static void fun() throws MyException2, MyException{
+        System.out.println("hahah");
+    }
+
+}
