6c5b056d32082559a4615276f10849246d2767cb
leilvlong <1455013965@qq.com>
2019-05-26 10:44:02 +0800

    异常捕获后重新抛出异常

---
+++ demo01/src/com/github/job09/job07.java
@@ -0,0 +1,57 @@
+package com.github.job09;
+
+public class job07 {
+    public static void main(String[] args) {
+        /*try{
+            Rethrowing.fun();
+        }catch (Exception e){
+            System.out.println("main: printStackTrace");
+            ErrorUtil.pringErrorMassage(e);
+        }
+        System.out.println("---------------------------------------");
+        try{
+            Rethrowing.gun();
+        }catch (Exception e){
+            System.out.println("main: printStackTrace");
+            ErrorUtil.pringErrorMassage(e);
+        }*/
+        System.out.println("---------------------------------------");
+        try{
+            Rethrowing.hun();
+        }catch (Exception e){
+            System.out.println("main: printStackTrace");
+            ErrorUtil.pringErrorMassage(e);
+        }
+
+
+    }
+}
+
+
+class Rethrowing{
+    public static void fun() throws Exception{
+        System.out.println("error in class Rethrowing method fun");
+        throw new NullPointerException("Throw from fun");
+    }
+
+    public static void gun() throws Exception {
+        try{
+            fun();
+        }catch (Exception e){
+            System.out.println("error in class Rethrowing method fun");
+            ErrorUtil.pringErrorMassage(e);
+            throw (Exception) e.fillInStackTrace();
+        }
+    }
+
+    public static void hun() throws Exception {
+        try{
+            gun();
+        }catch (Exception e){
+            System.out.println("error in class Rethrowing method fun");
+            ErrorUtil.pringErrorMassage(e);
+            throw (Exception) e.fillInStackTrace();
+        }
+    }
+
+}
