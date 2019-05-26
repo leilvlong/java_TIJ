bba89a8f3c8ceeb11996d10ad24d17a4ca5a83a3
leilvlong <1455013965@qq.com>
2019-05-26 11:07:44 +0800

    异常捕获

---
+++ demo01/src/com/github/job09/job08.java
@@ -0,0 +1,44 @@
+package com.github.job09;
+
+/*
+out error仅知道自己来自于main方法在第十三行发生的异常
+每个异常都是如此,仅知道自己发生异常的信息
+异常对象都是new出来的,垃圾回收器会自动回收
+ */
+
+public class job08 {
+    public static void main(String[] args) {
+        try{
+            try{
+                RethrowNew.fun();
+            }catch (OneException e){
+                System.out.println("inner error");
+                ErrorUtil.pringErrorMassage(e);
+                throw new TwoException("throw from class job08 method main");
+            }
+        }catch (TwoException e){
+            System.out.println("out error");
+            ErrorUtil.pringErrorMassage(e);
+        }
+    }
+}
+
+class OneException extends Exception{
+    public OneException(String message) {
+        super(message);
+    }
+}
+
+
+class TwoException extends Exception{
+    public TwoException(String message) {
+        super(message);
+    }
+}
+
+
+class RethrowNew{
+    public static void fun() throws OneException {
+        throw new OneException("throw from class RethrowNew method fun");
+    }
+}
