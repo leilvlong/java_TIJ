1c4433c4c086d9a5fc09062f19f62c228b05f514
leilvlong <1455013965@qq.com>
2019-05-26 10:06:11 +0800

    ErrorLog输出

--- demo01/src/com/github/job09/ErrorUtil.java
+++ demo01/src/com/github/job09/ErrorUtil.java
@@ -13,7 +13,6 @@ class ErrorUtil{
     public static void pringErrorMassage(Exception e) {
         try{
             PrintStream ps = new PrintStream(new FileOutputStream(logErrorPath, true));
-            ps.write(getErrorDate());
             ps.write(getErrorMassage(e));
             ps.close();
         }catch (Exception ee){
@@ -22,18 +21,19 @@ class ErrorUtil{
 
     }
 
-    private static byte[] getErrorDate(){
+    private static String getErrorDate(){
         long l = System.currentTimeMillis();
         Date date = new Date(l);
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd/HH:mm:ss");
         String errorDate = "errorDateTime:\n     "+simpleDateFormat.format(date)+"\n";
-        return errorDate.getBytes();
+        return errorDate;
     }
 
     private static byte[] getErrorMassage(Exception e){
         sb = new StringBuilder();
         StackTraceElement ste = e.getStackTrace()[0];
-        sb.append("errorMassage:\n    ").append("FileLocation: ");
+        sb.append("----------------------------WARNING----------------------------\n").
+                append(getErrorDate()).append("errorMassage:\n    ").append("FileLocation: ");
 
         String[] path = ste.getClassName().split("\\.");
         for (int i = 0; i < path.length-1; i++) {
@@ -42,10 +42,8 @@ class ErrorUtil{
         String errorMassage = sb.append(ste.getFileName()).append("\n    ").
                 append("ClassName: ").append(path[path.length - 1]).append("\n    ").
                 append("MethodName: ").append(ste.getMethodName()).append("\n    ").
-                append("ErrorLineNumber: ").append(ste.getLineNumber()).append("\n    massage: ").
-                append(e.getMessage() == null ? e.getClass().getName() : e.toString()).append("\n").
-                append("-------------------------------------------------------------\n").toString();
-
+                append("ErrorLineNumber: ").append(ste.getLineNumber()).append("\n    MASSAGE: ").
+                append(e.getMessage() == null ? e.getClass().getName() : e.toString()).append("\n").toString();
         return errorMassage.getBytes();
     }
 }
