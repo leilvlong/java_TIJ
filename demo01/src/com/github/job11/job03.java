4d72353c4c4a0e311cf6a09ccc4f5bf46519b8fd
leilvlong <1455013965@qq.com>
2019-06-14 17:42:07 +0800

    类字面常量(static final)

--- demo01/src/com/github/job11/job03.java
+++ demo01/src/com/github/job11/job03.java
@@ -13,6 +13,9 @@ package com.github.job11;
     如果一个static final的值是编译期常量（在四类八种的取值范围内），
     那么不需要被初始化就可以被读取,该类的静态域自然也不会被初始化
 
+末尾附对应的jvm指令:
+    通过观看指令可以看到,jvm对编译期常量没有任何的操作
+    对非编译期常量有getstatic与pushstatic这两操作
  */
 
 
@@ -22,7 +25,7 @@ public class job03 {
     public static Random rand = new Random();
 
     public static void main(String[] args) throws IllegalAccessException, InstantiationException {
-        //值是常量,编译器以确定,读取即可,不会初始化
+        //值是常量,编译期以确定,读取即可,不会初始化
         System.out.println(Initable1.staticFinal);
         //值在运行时由别的类的静态成员产生,非编译器常量,该类静态区域会被初始化
         System.out.println(Initable1.staticFinal2);
@@ -60,3 +63,68 @@ class Initable3{
         System.out.println("Initializing Initable3");
     }
 }
+
+
+/*
+class com.github.job11.Initable1 {
+  static final int staticFinal;
+
+  static final int staticFinal2;
+
+  com.github.job11.Initable1();
+    Code:
+       0: aload_0
+       1: invokespecial #1                  // Method java/lang/Object."<init>":()V
+       4: return
+
+  static {};
+    Code:
+       0: getstatic     #2                  // Field com/github/job11/job03.rand:Ljava/util/Random;
+       3: sipush        1000
+       6: invokevirtual #3                  // Method java/util/Random.nextInt:(I)I
+       9: putstatic     #4                  // Field staticFinal2:I
+      12: getstatic     #5                  // Field java/lang/System.out:Ljava/io/PrintStream;
+      15: ldc           #6                  // String Initializing Initable1
+      17: invokevirtual #7                  // Method java/io/PrintStream.println:(Ljava/lang/String;)V
+      20: return
+}
+
+
+class com.github.job11.Initable2 {
+  static final boolean staticFinal;
+
+  com.github.job11.Initable2();
+    Code:
+       0: aload_0
+       1: invokespecial #1                  // Method java/lang/Object."<init>":()V
+       4: return
+
+  static {};
+    Code:
+       0: getstatic     #2                  // Field java/lang/System.out:Ljava/io/PrintStream;
+       3: ldc           #3                  // String Initializing Initable2
+       5: invokevirtual #4                  // Method java/io/PrintStream.println:(Ljava/lang/String;)V
+       8: return
+}
+
+
+class com.github.job11.Initable3 {
+  static int staticFinal;
+
+  com.github.job11.Initable3();
+    Code:
+       0: aload_0
+       1: invokespecial #1                  // Method java/lang/Object."<init>":()V
+       4: return
+
+  static {};
+    Code:
+       0: bipush        74
+       2: putstatic     #2                  // Field staticFinal:I
+       5: getstatic     #3                  // Field java/lang/System.out:Ljava/io/PrintStream;
+       8: ldc           #4                  // String Initializing Initable3
+      10: invokevirtual #5                  // Method java/io/PrintStream.println:(Ljava/lang/String;)V
+      13: return
+}
+
+ */
