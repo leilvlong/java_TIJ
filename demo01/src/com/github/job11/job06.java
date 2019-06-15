a56caac3450539374c0458745d969f2880f5628c
leilvlong <1455013965@qq.com>
2019-06-15 10:50:05 +0800

    Class泛型引用 其三(下边界)

--- demo01/src/com/github/job11/job06.java
+++ demo01/src/com/github/job11/job06.java
@@ -12,31 +12,45 @@ Class<? super GrandSonClass>:
     所以使用newInstance()时统一指向Object类型引用的策略是合理的
     而且当getSuperclass()最终指向了Object.class后如果继续网上寻找或获得一个null
     同样,因为是直接创建的对象,所以不会维护向下转型的关系(不可向下强转为当前Class对象更低级的导出类)
+    这里并不冲突,Class对象依然处于泛型引用的下边界中,但通过Class对象创建的是类对象并非是Class对象
     以下示例将揭示这一特性
 */
 public class job06 {
     public static void main(String[] args) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         Class<GrandSonClass> grandSonClassClass = GrandSonClass.class;
         GrandSonClass grandSonClass = grandSonClassClass.newInstance();
-        System.out.println(grandSonClass);
+        System.out.println("创建一个 GrandSonClass 对象:  " + grandSonClass);
 
+        //尽管当前的下边界依然是GrandSonClass 但实际的Class对象则是SubClass 强转边界为SubClass
         Class<? super GrandSonClass> superclassForOne = grandSonClassClass.getSuperclass();
         Object objectForOne = superclassForOne.newInstance();
         SubClass subClass = (SubClass) objectForOne;
-        System.out.println(subClass);
+        System.out.println("创建一个 SubClass 对象:  " +subClass);
 
+        //尽管当前的下边界依然是GrandSonClass 但实际的Class对象则是BaseClass 强转边界为BaseClass
         Class<? super GrandSonClass> superclassForTwo = superclassForOne.getSuperclass();
         Object objectForTwo = superclassForTwo.newInstance();
         BaseClass baseClass = (BaseClass) objectForTwo;
-        System.out.println(baseClass);
+        System.out.println("创建一个 BaseClass 对象:  " +baseClass);
 
+        //尽管当前的下边界依然是GrandSonClass 但实际的Class对象则是Object 强转边界为 没有可以强转的余地了
         Class<? super GrandSonClass> superclassForThree = superclassForTwo.getSuperclass();
         Object objectForThree = superclassForThree.newInstance();
-        System.out.println(objectForThree);
+        System.out.println("创建一个 Object 对象:  " +objectForThree);
 
         /*Class<? super GrandSonClass> superclassForFour = superclassForThree.getSuperclass();
         Object objectForFour = superclassForFour.newInstance();
         System.out.println(superclassForFour);*/
+
+        System.out.println();
+        System.out.println("下边界中的获取的Class对象可以自由转换引用: Class<? super GrandSonClass>");
+        superclassForThree = superclassForTwo;
+        superclassForTwo = superclassForOne;
+        Object object = superclassForThree.newInstance();
+        System.out.println("superclassForThree = superclassForTwo; 创建对象: " +object);
+        Object object1 = superclassForTwo.newInstance();
+        System.out.println("superclassForTwo = superclassForOne; 创建对象: "+object1);
+
     }
 }
 
