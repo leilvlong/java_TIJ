3e2376b2ded40d3d0a6ad8b7e7817ebd15ea0f8f
leilvlong <1455013965@qq.com>
2019-07-09 08:44:58 +0800

    模板引擎的极其简单实现

---
+++ demo01/src/com/github/java12/TempLateTxt.java
@@ -0,0 +1,58 @@
+package com.github.java12;
+/*
+最低限度的实现模板引擎
+ */
+
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TempLateTxt {
+    private static String templatPath = "demo01/funs/template.txt";
+    private static String outPath = "demo01/funs/templateOut.txt";
+    private static String regex = ".*\\{(.*)}.*";
+
+    private static String replaceKey(String key) {
+        return "\\$\\{" + key + "}";
+    }
+
+
+    private static Map<String, String> replaceData;
+
+    static {
+        replaceData = new HashMap<>();
+        replaceData.put("name", "hello word");
+        replaceData.put("open", "happy");
+        replaceData.put("status","大雨");
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        FileReader fileReader = new FileReader(new File(templatPath));
+        FileWriter writer = new FileWriter(new File(outPath));
+
+        int i = 0;
+        char[] chars = new char[1024];
+        while ((i = fileReader.read(chars)) != -1) {
+            String s = new String(chars, 0, chars.length);
+            String[] split = s.split("\\$");
+            for (String filed : split) {
+                Matcher matcher = Pattern.compile(regex).matcher(filed);
+                while (matcher.find()) {
+                    String key = matcher.group(1);
+                    String data = replaceData.get(key);
+                    String regexKey = replaceKey(key);
+                    s = s.replaceFirst(regexKey,data);
+                }
+            }
+            writer.write(s);
+        }
+        writer.close();
+        fileReader.close();
+
+    }
+}
