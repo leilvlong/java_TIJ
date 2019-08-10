29998021d478efdb4ffa2d40dbce3213f529799f
leilvlong <1455013965@qq.com>
2019-08-10 22:18:39 +0800

    模板引擎的简单实现

--- demo01/src/com/github/java12/TempLateTxt.java
+++ demo01/src/com/github/java12/TempLateTxt.java
@@ -28,6 +28,7 @@ public class TempLateTxt {
         replaceData.put("name", "hello word");
         replaceData.put("open", "happy");
         replaceData.put("status","大雨");
+        replaceData.put("with","可以的!!!");
 
     }
 
@@ -38,19 +39,22 @@ public class TempLateTxt {
         int i = 0;
         char[] chars = new char[1024];
         while ((i = fileReader.read(chars)) != -1) {
-            String s = new String(chars, 0, chars.length);
+            String s = new String(chars, 0, i);
             String[] split = s.split("\\$");
-            for (String filed : split) {
-                Matcher matcher = Pattern.compile(regex).matcher(filed);
-                while (matcher.find()) {
-                    String key = matcher.group(1);
-                    String data = replaceData.get(key);
-                    String regexKey = replaceKey(key);
-                    s = s.replaceFirst(regexKey,data);
+            if (split.length>1){
+                for (String filed : split) {
+                    Matcher matcher = Pattern.compile(regex).matcher(filed);
+                    while (matcher.find()) {
+                        String key = matcher.group(1);
+                        String data = replaceData.get(key);
+                        String regexKey = replaceKey(key);
+                        s = s.replaceFirst(regexKey,data);
+                    }
                 }
             }
             writer.write(s);
         }
+
         writer.close();
         fileReader.close();
 
