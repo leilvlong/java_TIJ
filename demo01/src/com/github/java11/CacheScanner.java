196eddf62c6e00337c58166503766e5bbb551465
leilvlong <1455013965@qq.com>
2020-04-01 21:03:48 +0800

    java I/O 仿事务模型

--- demo01/src/com/github/java11/CacheScanner.java
+++ demo01/src/com/github/java11/CacheScanner.java
@@ -1,17 +1,19 @@
 package com.github.java11;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * 利用java I/O 仿事务
+ * 利用java I/O 仿事务模型
  */
 
 public class CacheScanner {
 
     private static boolean isCommit = true;
+
+    private static Map<String,Integer> rollbackMap = new HashMap<>();
 
     public static void main(String[] args) throws FileNotFoundException {
         // 输入
@@ -27,16 +29,52 @@ public class CacheScanner {
         try{
             while (scanner.hasNext()){
                 String s = scanner.nextLine();
+
+                /**
+                 * 命令区
+                 */
+
+                // 停止执行指令
                 if (s.equals("break"))
                     break;
 
+                // 出现异常回滚指令
                 if (s.equals("error"))
                     throw new RuntimeException();
 
+                // 设置rollback回滚点指令
+                if (s.matches("setIsCommit\\(.*\\)")) {
+                    String param = matcherParam(s);
+                    setIsCommit(param, data);
+                    s = null;
+                }
+                // 回到指定回滚点指令
+                else if (s.matches("rollback\\(.*\\)")){
+                    String param = matcherParam(s);
+                    data = rollback(param, data);
+                    s = null;
+                }
+
+                /**
+                 * 提交区
+                 */
+
+                // 自动提交
                 if (isCommit)
                     commit(outputStream, s);
-                else
-                    data.add(s);
+
+                // 指定回滚点提交指令
+                else if(s != null && s.matches("commit\\(.*\\)")){
+                    String param = matcherParam(s);
+                    commit(param,data,outputStream);
+                }
+
+                // 不提交,添加到缓存区
+                else{
+                    if (s != null){
+                        data.add(s);
+                    }
+                }
             }
             commit(outputStream, data);
         }catch (Exception E){
@@ -48,6 +86,11 @@ public class CacheScanner {
 
     public static void setIsCommit(boolean isCommit){
         CacheScanner.isCommit = isCommit;
+    }
+
+    public static void setIsCommit(String rollbackPoint, List<String> data){
+        rollbackMap.put(rollbackPoint, data.size());
+        CacheScanner.isCommit = false;
     }
 
     public static void commit(OutputStream printStream, List<String> data) throws IOException {
@@ -62,9 +105,54 @@ public class CacheScanner {
         printStream.write("\n".getBytes());
     }
 
+    public static void commit(String rollbackPoint,List<String> data, OutputStream printStream) throws IOException {
+        Integer rollbackIndex = rollbackMap.get(rollbackPoint);
+        for (Integer i = 0; i < rollbackIndex; i++) {
+            printStream.write(data.get(i).getBytes());
+            printStream.write("\n".getBytes());
+        }
+        data.clear();
+    }
+
     public static void rollback(List<String> data){
         System.out.println("出现异常 回滚");
         data.clear();
     }
 
+    public static List<String> rollback(String rollbackPoint, List<String> data){
+        Integer rollbackIndex = rollbackMap.get(rollbackPoint);
+        return data.subList(0,rollbackIndex );
+
+    }
+
+    public static String matcherParam(String method){
+        Matcher matcher = Pattern.compile(".*\\((.*)\\)").matcher(method);
+        while (matcher.find()){
+            return matcher.group(1);
+        }
+
+        return method;
+    }
+
 }
+
+class Mat{
+    public static void main(String[] args) {
+        String str = "revome(hello)" ;
+        String reg = ".*\\((.*)\\)";
+
+
+        String str1 = "#{test}";
+        String reg1 = ".*\\{(.*)\\}";
+
+
+        Matcher matcher = Pattern.compile(reg1).matcher(str1);
+
+        while (matcher.find()){
+            System.out.println(matcher.group(1));
+        }
+
+    }
+}
+
+
